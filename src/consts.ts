58bb1a23d498d40c1716043587b7e7e81d7742d5
AndDev <23031429@itcelaya.edu.mx>
2025-05-20 23:02:49 -0600

    feat: Update site constants and add new technologies
    - Updated the WORK page title to "Work and Certifications".
    - Added a new technology "Socket.io" to the stack in the index page.
    - Added a new certification for "React: Aplicaciones en tiempo real con Socket-io" in the work page.
    - Improved formatting and consistency in the consts.ts file.

--- src/consts.ts
+++ src/consts.ts
@@ -1,81 +1,81 @@
-import type { Site, Page, Links, Socials } from "@types"
+import type { Site, Page, Links, Socials } from "@types";
 
 // Global
 export const SITE: Site = {
   TITLE: "CastDev-J | Front-Developer",
-  DESCRIPTION: "Welcome to my personal website. I am a frontend developer with a passion for web development and design.",
+  DESCRIPTION:
+    "Welcome to my personal website. I am a frontend developer with a passion for web development and design.",
   AUTHOR: "CastDev-J",
-}
+};
 
 // Work Page
 export const WORK: Page = {
-  TITLE: "Work",
+  TITLE: "Work and Certifications",
   DESCRIPTION: "Places I have worked.",
-}
+};
 
 // Blog Page
 export const BLOG: Page = {
   TITLE: "Blog",
   DESCRIPTION: "Writing on topics I am passionate about.",
-}
+};
 
-// Projects Page 
+// Projects Page
 export const PROJECTS: Page = {
   TITLE: "Projects",
   DESCRIPTION: "Recent projects I have worked on.",
-}
+};
 
 // Search Page
 export const SEARCH: Page = {
   TITLE: "Search",
   DESCRIPTION: "Search all posts and projects by keyword.",
-}
+};
 
 // Links
 export const LINKS: Links = [
-  { 
-    TEXT: "Home", 
-    HREF: "/", 
+  {
+    TEXT: "Home",
+    HREF: "/",
   },
-  { 
-    TEXT: "Work", 
-    HREF: "/work", 
+  {
+    TEXT: "Work",
+    HREF: "/work",
   },
-  // { 
-  //   TEXT: "Blog", 
-  //   HREF: "/blog", 
+  // {
+  //   TEXT: "Blog",
+  //   HREF: "/blog",
   // },
-  { 
-    TEXT: "Projects", 
-    HREF: "/projects", 
+  {
+    TEXT: "Projects",
+    HREF: "/projects",
   },
-]
+];
 
 // Socials
 export const SOCIALS: Socials = [
-  { 
+  {
     NAME: "Email",
-    ICON: "email", 
+    ICON: "email",
     TEXT: "andres2004_cj@outlook.com",
     HREF: "mailto:andres2004_cj@outlook.com",
   },
-  { 
+  {
     NAME: "Github",
     ICON: "github",
     TEXT: "CastDev-j",
-    HREF: "https://github.com/CastDev-j"
+    HREF: "https://github.com/CastDev-j",
   },
-  { 
+  {
     NAME: "LinkedIn",
     ICON: "linkedin",
     TEXT: "andres-castillo",
     HREF: "https://www.linkedin.com/in/andres-castillo-jimenez-249210292/",
   },
-  { 
+  {
     NAME: "Twitter",
     ICON: "twitter-x",
     TEXT: "CastDev_J",
     HREF: "https://x.com/CastDev_J",
   },
-]
-
+];
